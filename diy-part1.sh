88166686c7f71f71bb892894de5ea864960ba8cd
xiaoyu20202020 <143551156+xiaoyu20202020@users.noreply.github.com>
2023-09-22 21:16:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,17 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#修改内核版本
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=5.4 /g' target/linux/ramips/Makefile
+
+#修改root密码为空
+#sed -i 's/root:$1$26qPRdbV$j9PQ1CCliyiydNI1uRxbH/:19598:0:99999:7:::/root:password/g' package/lean/default-settings/files/zzz-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme修改默认主题
+rm -rf package/lean/luci-theme-bootstrap
+# rm -rf package/lean/luci-theme-argon
+# find ./ -name luci-theme-argon | xargs rm -rf;
+# find ./ -name luci-app-argon-config | xargs rm -rf;
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
